751f87872842709153086e0b69764f9d02af3898
George Sotiropoulos <george@gsotworks.com>
2023-01-28 00:06:04 -0600

    Day 7: finished one course!

--- 01_FSjs/exampleApp/18_ExpressServer.js
+++ 01_FSjs/exampleApp/18_ExpressServer.js
@@ -23,17 +23,26 @@ app.use(express.json()); // now we enable cors and Express with .json files
 app.post('/run', // create a POST request for a new piece of data with the URI "/run"
   async (request, reply) => { // .post() lets us run a callback on arg #2 (also asynchronous from the ASYNC keyword)
     
-    const prompt = request.body.prompt; // grab our AI prompt from the request
-
-    const aiResponse = await openai.createImage({ // wait for openAI to generate a picture...
-        prompt, // ...using the prompt...
-        n: 1, // ...a specified amount...
-        size: '1024x1024' // ...and an image size
-    });
-
-    const image = aiResponse.data.data[0].url; // grab the URL of the finished AI generation!
+    try { // (SEE LESSON 22 BELOW)
     
-    reply.send({ image }); // send the link back to the client/browser in json format
+        const prompt = request.body.prompt; // grab our AI prompt from the request
+
+        const aiResponse = await openai.createImage({ // wait for openAI to generate a picture...
+            prompt, // ...using the prompt...
+            n: 1, // ...a specified amount...
+            size: '1024x1024' // ...and an image size
+        });
+
+        const image = aiResponse.data.data[0].url; // grab the URL of the finished AI generation!
+        
+        reply.send({ image }); // send the link back to the client/browser in json format
+    
+    } catch (error) { // (SEE LESSON 22 BELOW)
+
+        console.error(error);
+        reply.status(500).send(error?.response.data.error.message || 'Something went wrong');
+
+    }
 }); 
 
 app.listen(8080, () => console.log('NOW RUNNING ON http://localhost:8080/run')) // launch our Node server on port 8080
@@ -43,3 +52,26 @@ app.listen(8080, () => console.log('NOW RUNNING ON http://localhost:8080/run')) 
 // separation of concerns
 // to share with friends
 // to avoid leaking credentials         // <----------
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// LESSON 22: If we don't implement ERROR HANDLING, an unexpected response from OpenAI will break our server
+
+// Using try{} and catch(error){} blocks helps anticipate errors returned from API calls 
+
+// We can use console.error(error) to log our error to the console instead of crashing our entire app
+
+// reply.status(500) sends a default API code of 5xx signifying a server-side issue
+// .send(...) chained on top of that lets us directly grab OpenAI's error message and pass it along to the console
+// if we don't get anything from OpenAI (hence the ? in error?) then we can return an error string (|| "") instead of null
+
+// This is how we handle errors to prevent backend breakdown, check `main.js` for frontend error handling...
+
+// ALL DONE! You can bundle your app with `npm run build` and host it anywhere you want
+
+// Pop quiz: What is the usual way to handle errors in an async function?
+
+// catch()
+// try/catch                   // <------------
+// throw
+// just ignore errors
--- 01_FSjs/exampleApp/main.js
+++ 01_FSjs/exampleApp/main.js
@@ -1,4 +1,4 @@
-// (covered in Lesson 20)
+// (covering Lessons 20, 21, and parts of 22)
 
 import './style.css' // import our styles here (why not)
 
@@ -7,6 +7,8 @@ const form = document.querySelector('form'); // grab our <form> block
 form.addEventListener('submit', async (e) => { // listen for when the form is submitted, then run an async function
   
   e.preventDefault(); // forms refresh the page by default when submitted- calling this method on the event disables that
+
+  enterLoadState(); // (SEE LESSON 21 BELOW)
 
   const data = new FormData(form); // create a map-like object to extract the form data
 
@@ -20,11 +22,20 @@ form.addEventListener('submit', async (e) => { // listen for when the form is su
     })
   });
 
-  const { image } = await response.json(); // wait until the finished image url returns to us, and store it jQuery-style
+  if (response.ok) { // (SEE LESSON 22 BELOW)
 
-  const result = document.querySelector('#result'); // grab the page section for image insertion...
-  result.innerHTML = `<img src="${image}" width="512" />`; // ...and use jQuery to splice its embed into the HTML
+    const { image } = await response.json(); // wait until the finished image url returns to us, and store it jQuery-style
 
+    const result = document.querySelector('#result'); // grab the page section for image insertion...
+    result.innerHTML = `<img src="${image}" width="512" />`; // ...and use jQuery to splice its embed into the HTML
+
+  } else { // (SEE LESSON 22 BELOW)
+      const err = await response.text();
+      alert(err);
+      console.error(err);
+  }
+
+  exitLoadState(); // (SEE LESSON 21 BELOW)
 });
 
 // To get our app up and running, run `node 18_ExpressServer.js` in one terminal and `npm run dev` in another
@@ -35,3 +46,34 @@ form.addEventListener('submit', async (e) => { // listen for when the form is su
 // Object
 // Map             // <-------------
 // Set
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// LESSON 21: To create a simple LOADING STATE, we can alter our front-end to animate while waiting for OpenAI
+
+function enterLoadState() {
+  const button = document.querySelector('button');
+  button.disabled = true; // grab our button and disable clicking it
+  button.innerHTML = 'THINKING... <span class="spinner">⚙️</span>'; // use the prefab CSS to also animate a loading icon
+}
+
+function exitLoadState() {
+  const button = document.querySelector('button');
+  button.disabled = false; 
+  button.innerHTML = 'SEND TO AI'; // re-enable our button's clickability and original text once done loading
+}
+
+// Pop quiz: What state should a button typically be in when the app performs an async operation?
+
+// active                   // <----------- INCORRECT (thought it was because async means the app still responds)
+// disabled                 // <----------- CORRECT
+// hidden
+// chillin
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// LESSON 22 CONTINUED: For frontend ERROR HANDLING, response.ok is a useful boolean to check if the API returned 200 OK
+
+// If response.ok returns false, use response.text to grab OpenAI's error message and display it on-screen with alert()
+
+// (ok go back to 18_ExpressServer.js now)
